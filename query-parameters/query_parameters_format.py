ab3d3162a140878675e5e3798e374e459242472b
carloslme <carloslmescom@gmail.com>
2022-10-20 15:25:02 -0500

    add query parameters format

--- query-parameters/query_parameters_format.py
+++ query-parameters/query_parameters_format.py
@@ -15,3 +15,10 @@ async def get_user(format: UsersFormat):
 @app.get("/user")
 async def get_user_ad(page: int=Query(1, gt=0), size:int=Query(10, le=100)):
     return {"page":page, "size":size}
+
+@app.get("/sellers")
+async def get_sellers(format: UsersFormat, page: int=Query(1, gt=0), size:int=Query(10, le=100)):
+    """
+    http://localhost:8000/sellers?format=short&page=5&size=50
+    """
+    return {"format":format, "page":page, "size":size}
